a7e78e4ba36cf241eb7000b31c23eaa77a64db98
Trey S <fjschmidt292@gmail.com>
2022-05-26 13:10:31 -0400

    Association work, needs a test

--- models/index.js
+++ models/index.js
@@ -3,11 +3,20 @@ const Menu = require('./Menu');
 const Recipe = require('./Recipe');
 
 Menu.hasMany(Recipe, {
-  foreignKey: 'menu_id',
+  foreignKey: 'Recipe_id',
 });
 
-Recipe.belongsTo(Menu, {
-  foreignKey: 'menu_id',
+Recipe.belongsToMany(Menu, {
+  foreignKey: 'Recipe_name',
+  as: 'recipe',
+});
+
+User.hasMany(Recipe, {
+  foreignKey: 'recipe_id',
+});
+
+Recipe.belongsTo(User, {
+    foreignKey: 'recipe_id',
 });
 
 module.exports = { User, Menu, Recipe };
