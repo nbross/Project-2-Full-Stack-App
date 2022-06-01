4aebae39246fa73d760f1c93d4b4ed32e0da0cb7
nbross <nbross2001@gmail.com>
2022-05-31 20:06:44 -0400

    updated home-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -131,21 +131,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     User.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
+        where: {
+            id: req.params.id
         }
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,8 +27,8 @@ router.get('/', (req, res) => {
             }
         ]
     })
-        .then(dbPostData => {
-            const menus = dbPostData.map(post => post.get({ plain: true }));
+        .then(dbMenuData => {
+            const menus = dbMenuData.map(menu => menu.get({ plain: true }));
 
             res.render('homepage', { menus });
         })
@@ -46,5 +46,52 @@ router.get('/login', (req, res) => {
 
     res.render('login');
 });;
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Recipe,
+                attributes: ['id', 'recipe', 'filename', 'description'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbMenuData => {
+            if (!dbMenuData) {
+                res.status(404).json({ message: 'No Menu Post found with this id' });
+                return;
+            }
+            // serialize the data
+            const post = dbMenuData.get({ plain: true })
+
+            // pass data to template
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn,
+                username: req.session.username
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
