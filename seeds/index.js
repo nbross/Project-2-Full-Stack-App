3770d24b63c866c40faa7b26df5d95e92ef6f7c0
Trey S <fjschmidt292@gmail.com>
2022-06-03 13:30:54 -0400

    merged seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,7 @@
 const sequelize = require('../config/connection');
 const seedMenu = require('./menuData');
 const seedRecipe = require('./recipeData');
-<<<<<<< HEAD
-=======
 const seedUser = require('./userData');
->>>>>>> seeds
 
 const seedAll = async () =>{
     await sequelize.sync({force: true});
