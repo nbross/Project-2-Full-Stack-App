cb4e9a10a29e2ec5c36c58b2d451ead6a393c33f
Trey S <fjschmidt292@gmail.com>
2022-06-06 11:20:02 -0400

    recipes not creating, need to get that sorted before implementing npm

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -5,31 +5,6 @@ const withAuth = require('../../utils/auth');
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage({
-    distination: (req, file, cb) => {
-        cb(null, '../../images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
-
-    }
-})
-
-const upload = multer({
-    storage: storage,
-    limit: {fileSize: '1000000'},
-    fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png|gif/
-        const mimeType = fileTypes.test(file.mimetype)
-        const extname = fileTypes.test(path.extname(file.originalname))
-
-        if(mimeType && extname) {
-            return cb(null, true)
-        }
-        cb('Please use a jpeg, jpg, png, or gif filetype')
-    }
-}).single('filename')
-
 //router.post('/', )
 
 router.get('/', (req, res) => {
@@ -90,12 +65,12 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/add', withAuth, upload, (req, res) => {
+router.post('/', withAuth, (req, res) => {
         Recipe.create({
             recipe_text: req.body.recipe_text,
             user_id: req.session.user_id,
             menu_id: req.body.menu_id,
-            filename: upload(req.body.filename)
+            filename: req.body.filename
         })
             .then(dbRecipeData => res.json(dbRecipeData))
             .catch(err => {
@@ -149,4 +124,3 @@ router.delete('/:id', withAuth, (req, res) => {
 
 
 module.exports = router;
-module.exports = upload;
