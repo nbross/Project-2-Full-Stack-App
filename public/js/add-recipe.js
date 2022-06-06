0f69de44098b67dbdb0d7892385511b49ed634e1
Trey S <fjschmidt292@gmail.com>
2022-06-06 14:44:26 -0400

    bug fixes

--- public/js/add-recipe.js
+++ public/js/add-recipe.js
@@ -1,37 +1,11 @@
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-    distination: (req, file, cb) => {
-        cb(null, '../../images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
-
-    }
-})
-
-const upload = multer({
-    storage: storage,
-    limit: {fileSize: '1000000'},
-    fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png|gif/
-        const mimeType = fileTypes.test(file.mimetype)
-        const extname = fileTypes.test(path.extname(file.originalname))
-
-        if(mimeType && extname) {
-            return cb(null, true)
-        }
-        cb('Please use a jpeg, jpg, png, or gif filetype')
-    }
-}).single('filename')
-
 async function newRecipeHandler(event) {
     event.preventDefault();
 
     const recipe_title = document.querySelector('input[name="recipe-title"]').value;
     const recipe_text = document.querySelector('input[name="recipe-text"]').value;
-    const filename = upload(document.querySelector(`input[name="filename"]`).value);
+    const filename = document.querySelector(`input[name="filename"]`).value;
     const menu_id = 1;
+    const user_id = 1;
 
     const response = await fetch(`/api/recipes`, {
         method: 'POST',
@@ -39,7 +13,8 @@ async function newRecipeHandler(event) {
             recipe_title,
             recipe_text,
             menu_id,
-            filename,   
+            filename, 
+            user_id,  
         }),
         headers: {
             'Content-Type': 'application/json'
@@ -48,6 +23,7 @@ async function newRecipeHandler(event) {
 
     if (response.ok) {
         document.location.replace('/dashboard');
+        console.log(filename)
     } else {
         alert(response.statusText);
     }
